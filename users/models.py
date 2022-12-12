b3f5ab48413d66af4f1f7af87879c8a923b0b2d4
Jorge Kimura <kimurajorge2001@gmail.com>
2022-12-12 14:58:10 -0300

    feat: tarefa 1 em andamento

--- users/models.py
+++ users/models.py
@@ -5,3 +5,7 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
+    email = models.EmailField(
+        unique=True,
+        error_messages="user with this email already exists.",
+    )
